622cd2d114f551363f164d7ffd44e339e025ec00
Cameron H <cameronholiman@gmail.com>
2019-02-10 14:17:29 -0700

    Never started the server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('express')()
+const http = require('http')
 const proxy = require('express-http-proxy')
 const config = require('./config')
 const deviceController = require('./hardware/deviceController')
@@ -8,5 +9,7 @@ app.use('/dwelo-proxy', proxy(config.dwelo.baseUrl))
 for(let id of Object.keys(config.devices)) {
     deviceController.addDevice(id, config.devices[id])
 }
+
+http.createServer(app).listen(config.port || 9090);
 
 require('./logparse')();
