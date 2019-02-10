ae0cddf12da1c53137db78c7e341234b02213171
Cameron H <cameronholiman@gmail.com>
2019-02-10 14:11:31 -0700

    Proxy didn't work

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require('express')
+const app = require('express')()
 const proxy = require('express-http-proxy')
 const config = require('./config')
 const deviceController = require('./hardware/deviceController')
 
-let app = express()
-
-proxy('/dwelo-proxy', config.dwelo.baseUrl);
+app.use('/dwelo-proxy', proxy(config.dwelo.baseUrl))
 
 for(let id of Object.keys(config.devices)) {
     deviceController.addDevice(id, config.devices[id])
