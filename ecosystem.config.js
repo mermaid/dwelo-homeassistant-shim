c4459adc44d7ab21089d7a711fe23c526e222a11
Cameron H <cameronholiman@gmail.com>
2019-02-10 13:43:14 -0700

    adding pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "apps" : [{
+    "name"        : "dwelo-shim",
+    "script"      : "npm run start",
+    "watch"       : true,
+    "log_file": "dwelo-shim.log",
+    "env": {
+      "NODE_ENV": "development"
+    },
+    "env_production" : {
+       "NODE_ENV": "production"
+    }
+  }]
+}
